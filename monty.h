da209c1f00719f961157275ee591d607845f9313
godstime01 <youngestkid01@gmail.com>
2023-01-19 02:13:01 -0800

    header file

---
+++ monty.h
@@ -0,0 +1,58 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_t - doubly linked list for respresentation of a stack( or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack
+ * @next: node that points to the next element  in the stack
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+d */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t, unsigned int line_number);
+} instruction_t;
+
+void _push(stack_t **head, unsigned int number);
+void _pall(stack_t **head, unsigned int number);
+void _pint(stack_t **head, unsigned int number);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void _pop(stack_t **head, unsigned int counter);
+void _swap(stack_t **head, unsigned int counter);
+void _add(stack_t **head, unsigned int counter);
+void _nop(stack_t **head, unsigned int counter);
+void _sub(stack_t **head, unsigned int counter);
+void _div(stack_t **head, unsigned int counter);
+void _mul(stack_t **head, unsigned int counter);
+void _mod(stack_t **head, unsigned int counter);
+void _pchar(stack_t **head, unsigned int counter);
+void _pstr(stack_t **head, unsigned int counter);
+void _rotl(stack_t **head, unsigned int counter);
+void _rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void _queue(stack_t **head, unsigned int counter);
+void _stack(stack_t **head, unsigned int counter);
+#endif
